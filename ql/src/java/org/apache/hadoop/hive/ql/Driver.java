b89dc33bfab35447c7b087203c832e77d1108c25
Gunther Hagleitner <gunther@apache.org>
2014-09-11 00:28:40 -0700

    HIVE-8042: Optionally allow move tasks to run in parallel (backport)

--- ql/src/java/org/apache/hadoop/hive/ql/Driver.java
+++ ql/src/java/org/apache/hadoop/hive/ql/Driver.java
@@ -48,6 +48,7 @@ import org.apache.hadoop.hive.metastore.api.Schema;
 import org.apache.hadoop.hive.ql.exec.ConditionalTask;
 import org.apache.hadoop.hive.ql.exec.ExecDriver;
 import org.apache.hadoop.hive.ql.exec.FetchTask;
+import org.apache.hadoop.hive.ql.exec.MoveTask;
 import org.apache.hadoop.hive.ql.exec.Operator;
 import org.apache.hadoop.hive.ql.exec.TableScanOperator;
 import org.apache.hadoop.hive.ql.exec.Task;
@@ -1348,10 +1349,17 @@ public class Driver implements CommandProcessor {
     TaskRunner tskRun = new TaskRunner(tsk, tskRes);
 
     // Launch Task
-    if (HiveConf.getBoolVar(conf, HiveConf.ConfVars.EXECPARALLEL) && tsk.isMapRedTask()) {
+    if (HiveConf.getBoolVar(conf, HiveConf.ConfVars.EXECPARALLEL)
+        && (tsk.isMapRedTask() || (tsk instanceof MoveTask))) {
       // Launch it in the parallel mode, as a separate thread only for MR tasks
+      if (LOG.isInfoEnabled()){
+        LOG.info("Starting task [" + tsk + "] in parallel");
+      }
       tskRun.start();
     } else {
+      if (LOG.isInfoEnabled()){
+        LOG.info("Starting task [" + tsk + "] in serial mode");
+      }
       tskRun.runSequential();
     }
     running.put(tskRes, tskRun);
