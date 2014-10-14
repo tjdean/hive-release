e8c6708beb21de5f3d8fbbd927ff4d7fcf375b85
Matt McCline <mmccline@hortonworks.com>
2014-10-13 18:34:47 -0700

    BUG-24872: (2.1) hive vectorization group by on date type partition of ORC table returns NULL for date type column value
    (Backport of HIVE-8434: Vectorization logic using wrong values for DATE and TIMESTAMP partitioning columns in vectorized row batches...)
    
    Ported by:
      Applying a modified patch HIVE-8434.02.patch that removes the change to testconfiguration.xml
         and manually adding vector_partitioned_date_time.q to itest/qtest/pom.xml
      Simplified vector_partitioned_date_time.q by removing sort by queries leaving just
         the group by queries.
    
      (adding this to ~/.m2/settings.xml):
    
    <?xml version="1.0" encoding="UTF-8"?>
    <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
          xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd">
     <profiles>
    
       <profile>
         <id>hwx</id>
         <repositories>
           <repository>
             <id>hwx</id>
             <name>hwx</name>
             <url>http://54.235.92.15/nexus/content/groups/public/</url>
           </repository>
         </repositories>
       </profile>
    
       <profile>
         <id>hwx2</id>
         <repositories>
           <repository>
             <id>hwx2</id>
             <name>hwx2</name>
             <url>http://sijenkinsservice.cloudapp.net:8081/nexus/content/repositories/hdp-baikal-nightly/</url>
           </repository>
         </repositories>
       </profile>
    
       <profile>
         <id>hwx3</id>
         <repositories>
           <repository>
             <id>hwx3</id>
             <name>hwx3</name>
             <url>http://sijenkinsservice.cloudapp.net:8081/nexus/content/groups/hdp-win-baikal-ut/</url>
           </repository>
         </repositories>
       </profile>
    
     </profiles>
    </settings>
    
      mvn clean install -Phadoop-2,hwx -DskipTests -Dhadoop-23.version=2.4.0.2.1.6.0-700
      mvn test -Dtest=TestCliDriver -Dqfile=vector_partitioned_date_time.q -Dtest.output.overwrite=true -Dhadoop-23.version=2.4.0.2.1.6.0-700 -Phadoop-2,hwx
      mvn test -Dtest=TestMiniTezCliDriver -Dqfile=vector_partitioned_date_time.q -Dtest.output.overwrite=true -Dhadoop-23.version=2.4.0.2.1.6.0-700 -Phadoop-2,hwx

--- ql/src/java/org/apache/hadoop/hive/ql/exec/vector/VectorizedRowBatchCtx.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/vector/VectorizedRowBatchCtx.java
@@ -44,6 +44,7 @@ import org.apache.hadoop.hive.serde2.ColumnProjectionUtils;
 import org.apache.hadoop.hive.serde2.Deserializer;
 import org.apache.hadoop.hive.serde2.SerDeException;
 import org.apache.hadoop.hive.serde2.SerDeUtils;
+import org.apache.hadoop.hive.serde2.io.DateWritable;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorConverters;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
@@ -469,7 +470,7 @@ public class VectorizedRowBatchCtx {
             lcv.isNull[0] = true;
             lcv.isRepeating = true;
           } else { 
-            lcv.fill(((Date)value).getTime());
+            lcv.fill(DateWritable.dateToDays((Date) value));
             lcv.isNull[0] = false;
           }          
         }
@@ -482,7 +483,7 @@ public class VectorizedRowBatchCtx {
             lcv.isNull[0] = true;
             lcv.isRepeating = true;
           } else { 
-            lcv.fill((long)(((Timestamp) value).getTime()));
+            lcv.fill(TimestampUtils.getTimeNanoSec((Timestamp) value));
             lcv.isNull[0] = false;
           }
         }
