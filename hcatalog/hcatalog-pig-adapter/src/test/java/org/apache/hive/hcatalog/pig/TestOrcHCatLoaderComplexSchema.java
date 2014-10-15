981d0bf16fe16c8deea538c4965f258f41ce16ed
Daniel Dai <daijy@hortonworks.com>
2014-10-14 21:48:12 -0700

    Revert "Rollback testcase BUG-19411: Null map values in ORC table gives PIG error ERROR 6018: Error converting read value to tuple"
    This reverts commit fc97b61eecfca72b0310be61bdfe019615a86db9.

--- hcatalog/hcatalog-pig-adapter/src/test/java/org/apache/hive/hcatalog/pig/TestOrcHCatLoaderComplexSchema.java
+++ hcatalog/hcatalog-pig-adapter/src/test/java/org/apache/hive/hcatalog/pig/TestOrcHCatLoaderComplexSchema.java
@@ -18,11 +18,40 @@
  */
 package org.apache.hive.hcatalog.pig;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.pig.data.Tuple;
+import org.junit.Test;
+
 public class TestOrcHCatLoaderComplexSchema extends TestHCatLoaderComplexSchema {
 
   @Override
   protected String storageFormat() {
     return "orc";
   }
+  /**
+   * artificially complex nested schema to test nested schema conversion
+   * @throws Exception
+   */
+  @Test
+  public void testMapNullKey() throws Exception {
+    String pigSchema = "m:map[]";
 
+    String tableSchema = "m map<string, string>";
+
+    List<Tuple> data = new ArrayList<Tuple>();
+    Tuple t = t(
+      new HashMap<String, String>() {
+      {
+        put("ac test1", "test 1");
+        put("ac test2", "test 2");
+        put(null, "test 3");
+      };
+    });
+    data.add(t);
+    verifyWriteRead("testSyntheticComplexSchema", pigSchema, tableSchema, data, true);
+    verifyWriteRead("testSyntheticComplexSchema", pigSchema, tableSchema, data, false);
+  }
 }
