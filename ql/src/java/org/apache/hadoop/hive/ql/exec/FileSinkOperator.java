c35da4d96b07799cb3676499c980180c9295cf1f
Gunther Hagleitner <gunther@apache.org>
2015-04-15 17:56:44 -0700

    BUG-34602: FileSinkOperator does not pass in conf to serde.initialize ( HIVE-9651 ) (Gunther Hagleitner)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/FileSinkOperator.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/FileSinkOperator.java
@@ -335,7 +335,7 @@ public class FileSinkOperator extends TerminalOperator<FileSinkDesc> implements
       statsCollectRawDataSize = conf.isStatsCollectRawDataSize();
       statsFromRecordWriter = new boolean[numFiles];
       serializer = (Serializer) conf.getTableInfo().getDeserializerClass().newInstance();
-      serializer.initialize(null, conf.getTableInfo().getProperties());
+      serializer.initialize(hconf, conf.getTableInfo().getProperties());
       outputClass = serializer.getSerializedClass();
 
       // Timeout is chosen to make sure that even if one iteration takes more than
