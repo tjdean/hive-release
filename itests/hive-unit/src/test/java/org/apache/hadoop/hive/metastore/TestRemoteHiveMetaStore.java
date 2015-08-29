a6a48078068ea3cfa706262bac92b70c4b13fd25
Jason Dere <jdere@hortonworks.com>
2015-08-28 17:09:37 -0700

    Unit test for HIVE-10384

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/metastore/TestRemoteHiveMetaStore.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/metastore/TestRemoteHiveMetaStore.java
@@ -18,9 +18,14 @@
 
 package org.apache.hadoop.hive.metastore;
 
+import java.util.List;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
+import org.apache.hadoop.hive.metastore.api.MetaException;
 import org.apache.hadoop.hive.shims.ShimLoader;
+import org.apache.thrift.transport.TTransportException;
 
 
 public class TestRemoteHiveMetaStore extends TestHiveMetaStore {
@@ -53,5 +58,84 @@ public class TestRemoteHiveMetaStore extends TestHiveMetaStore {
     hiveConf.setVar(HiveConf.ConfVars.METASTOREURIS, "thrift://localhost:" + port);
     hiveConf.setBoolVar(ConfVars.METASTORE_EXECUTE_SET_UGI,setugi);
     client = new HiveMetaStoreClient(hiveConf);
+  }
+
+
+  // Metastore Client to simulate failed metastore requests
+  static class FailingHiveMetaStoreClient extends HiveMetaStoreClient implements IMetaStoreClient {
+
+    static int configuredFailures;
+    static int callCount;
+
+    public static int getConfiguredFailures() {
+      return configuredFailures;
+    }
+
+    public static void setConfiguredFailures(int newConfiguredFailures) {
+      configuredFailures = newConfiguredFailures;
+    }
+
+    public static int getCallCount() {
+      return callCount;
+    }
+
+    public static void setCallCount(int newCallCount) {
+      callCount = callCount;
+    }
+
+    public FailingHiveMetaStoreClient(HiveConf conf) throws MetaException {
+      super(conf);
+    }
+
+    public FailingHiveMetaStoreClient(HiveConf conf, HiveMetaHookLoader hookLoader) throws MetaException {
+      super(conf, hookLoader);
+    }
+
+    /**
+     * Override getAllDatabases() to fail a specified number of times, using configuredFailures
+     */
+    @Override
+    public List<String> getAllDatabases() throws MetaException {
+      ++callCount;
+      if (configuredFailures > 0 && configuredFailures >= callCount) {
+        String msg = "Test failure " + callCount;
+        Exception cause = new TTransportException(msg);
+        MetaException error = new MetaException(ExceptionUtils.getStackTrace(cause));
+        throw error;
+      }
+
+      return super.getAllDatabases();
+    }
+  }
+
+  private HiveMetaHookLoader getHookLoader() {
+    HiveMetaHookLoader hookLoader = new HiveMetaHookLoader() {
+      @Override
+      public HiveMetaHook getHook(
+          org.apache.hadoop.hive.metastore.api.Table tbl)
+          throws MetaException {
+        return null;
+      }
+    };
+    return hookLoader;
+  }
+
+  public void testRetryTTransportException() throws Throwable {
+    HiveConf conf = new HiveConf(hiveConf);
+    // Test a normal retriable client
+    IMetaStoreClient client =
+        RetryingMetaStoreClient.getProxy(conf, getHookLoader(), HiveMetaStoreClient.class.getName());
+    List<String> dbs = client.getAllDatabases();
+    int dbCount = dbs.size();
+    client.close();
+
+    FailingHiveMetaStoreClient.setConfiguredFailures(1);
+    FailingHiveMetaStoreClient.setCallCount(0);
+
+    // Now try with our failing client, set to fail for the first call to getAllDatabases()
+    client = RetryingMetaStoreClient.getProxy(conf, getHookLoader(), FailingHiveMetaStoreClient.class.getName());
+    dbs = client.getAllDatabases();
+    assertEquals(dbCount, dbs.size());
+    assertEquals(2, FailingHiveMetaStoreClient.getCallCount());
   }
 }
