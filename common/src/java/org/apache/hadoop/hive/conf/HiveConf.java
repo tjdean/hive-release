cc78322c41190eaee8163e9a5b5d42c3494028d2
Sushanth Sowmyan <khorgath@gmail.com>
2015-11-06 09:32:57 -0800

    [BUG-28778][HIVE-12345] SUMMARY-[Followup for HIVE-9013 : Hidden conf vars still visible through beeline]

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -2717,6 +2717,7 @@ public class HiveConf extends Configuration {
     auxJars = other.auxJars;
     origProp = (Properties)other.origProp.clone();
     restrictList.addAll(other.restrictList);
+    hiddenSet.addAll(other.hiddenSet);
     modWhiteListPattern = other.modWhiteListPattern;
   }
 
