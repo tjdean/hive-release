ab0c3081c76ece7f822d9221f80aa78f306b2d68
vikram <vikram@hortonworks.com>
2015-11-18 19:08:19 -0800

    BUG-47623/HIVE-12387: Bug with logging improvements in ATS (Vikram Dixit K, reviewed by Thejas Nair)

--- shims/0.23/src/main/java/org/apache/hadoop/hive/shims/Hadoop23Shims.java
+++ shims/0.23/src/main/java/org/apache/hadoop/hive/shims/Hadoop23Shims.java
@@ -1381,6 +1381,7 @@ public class Hadoop23Shims extends HadoopShimsSecure {
 
   @Override
   public void setHadoopCallerContext(String callerContext) {
+    callerContext = "HIVE_QUERY_ID:" + callerContext;
     CallerContext.setCurrent(new CallerContext.Builder(callerContext).build());
   }
 
