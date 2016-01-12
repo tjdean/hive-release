5cc2dce29f0bea9f70b3beb3e2cf11ecd9b2cdc0
Alan Gates <gates@hortonworks.com>
2016-01-12 10:57:50 -0800

    BUG-50428 Fixed issue in declaring proper schema version for new Hive 1.2.1000 version.

--- metastore/scripts/upgrade/mysql/hive-schema-1.2.1000.mysql.sql
+++ metastore/scripts/upgrade/mysql/hive-schema-1.2.1000.mysql.sql
@@ -824,7 +824,7 @@ SOURCE hive-txn-schema-1.2.1000.mysql.sql;
 -- -----------------------------------------------------------------
 -- Record schema version. Should be the last step in the init script
 -- -----------------------------------------------------------------
-INSERT INTO VERSION (VER_ID, SCHEMA_VERSION, VERSION_COMMENT) VALUES (1, '1.2.0', 'Hive release version 1.2.0');
+INSERT INTO VERSION (VER_ID, SCHEMA_VERSION, VERSION_COMMENT) VALUES (1, '1.2.1000', 'Hive release version 1.2.1000');
 
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
