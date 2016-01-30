cd4f5063a18cc3e002077610fb8b2e1a7e2de310
vikram <vikram@hortonworks.com>
2016-01-29 20:29:44 -0800

    BUG-51447/HIVE-12947: SMB mapjoin query runtime error "FileSinkOperator cannot be cast to org.apache.hadoop.hive.ql.exec.DummyStoreOperator" (Vikram Dixit K)

--- ql/src/java/org/apache/hadoop/hive/ql/exec/tez/MapRecordProcessor.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/tez/MapRecordProcessor.java
@@ -154,6 +154,7 @@ public class MapRecordProcessor extends RecordProcessor {
       mapOp.setExecContext(execContext);
 
       connectOps.clear();
+      boolean fromCache = false;
       if (mergeWorkList != null) {
         MapOperator mergeMapOp = null;
         for (BaseWork mergeWork : mergeWorkList) {
@@ -198,14 +199,15 @@ public class MapRecordProcessor extends RecordProcessor {
               mergeMapOp.setChildren(jconf);
             }
 
-            l4j.info("BUG-48404: Class cast for operator tree of Map Work: " +
-                mergeMapWork.getName() + " operator pipeline: ");
-            DummyStoreOperator dummyOp = getJoinParentOp(mergeMapOp);
-            if (dummyOp instanceof TezDummyStoreOperator) {
-              // we ensure that we don't try to read any data for this table.
-              ((TezDummyStoreOperator) dummyOp).setFetchDone(skipRead);
+            Operator<? extends OperatorDesc> finalOp = getFinalOp(mergeMapOp);
+            if (finalOp instanceof TezDummyStoreOperator) {
+              // we ensure that we don't try to read any data in case of skip read.
+              ((TezDummyStoreOperator) finalOp).setFetchDone(skipRead);
+              connectOps.put(mergeMapWork.getTag(), (DummyStoreOperator) finalOp);
+            } else {
+              // found the plan is already connected which means this is derived from the cache.
+              fromCache = true;
             }
-            connectOps.put(mergeMapWork.getTag(), dummyOp);
 
             mergeMapOp.passExecContext(new ExecMapperContext(jconf));
             mergeMapOp.initializeLocalWork(jconf);
@@ -213,7 +215,9 @@ public class MapRecordProcessor extends RecordProcessor {
         }
       }
 
-      ((TezContext) (MapredContext.get())).setDummyOpsMap(connectOps);
+      if (!fromCache) {
+        ((TezContext) (MapredContext.get())).setDummyOpsMap(connectOps);
+      }
 
       // initialize map operator
       mapOp.setConf(mapWork);
@@ -308,14 +312,12 @@ public class MapRecordProcessor extends RecordProcessor {
     return reader;
   }
 
-  private DummyStoreOperator getJoinParentOp(Operator<? extends OperatorDesc> mergeMapOp) {
-    l4j.info("BUG-48404: Operator is " + mergeMapOp + " is of type " +
-        mergeMapOp.getClass().getCanonicalName());
+  private Operator<? extends OperatorDesc> getFinalOp(Operator<? extends OperatorDesc> mergeMapOp) {
     for (Operator<? extends OperatorDesc> childOp : mergeMapOp.getChildOperators()) {
       if ((childOp.getChildOperators() == null) || (childOp.getChildOperators().isEmpty())) {
-        return (DummyStoreOperator) childOp;
+        return childOp;
       } else {
-        return getJoinParentOp(childOp);
+        return getFinalOp(childOp);
       }
     }
     return null;
