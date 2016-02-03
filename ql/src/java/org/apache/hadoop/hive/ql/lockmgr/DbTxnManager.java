33baadd0e998d36fc3da9ddb875ebf02d1e15460
Eugene Koifman <ekoifman@hortonworks.com>
2016-02-03 15:52:10 -0800

    BUG-62547 BUG-55547 HIVE-11716: Reading ACID table from non-acid session should raise an error (Wei Zheng via Eugene Koifman)
    (cherry-picked:4d486696a8e38891b46bb52eeb2d787696fad43e)

--- ql/src/java/org/apache/hadoop/hive/ql/lockmgr/DbTxnManager.java
+++ ql/src/java/org/apache/hadoop/hive/ql/lockmgr/DbTxnManager.java
@@ -332,6 +332,11 @@ public class DbTxnManager extends HiveTxnManagerImpl {
   }
 
   @Override
+  public String getTxnManagerName() {
+    return CLASS_NAME;
+  }
+
+  @Override
   public boolean supportsExplicitLock() {
     return false;
   }
