tjdean/hive-release

commit ce8bf5445be00b6d9f95dffbb5affbd7daf42fa6

HIVE-12967: Change LlapServiceDriver to read a properties file instead of llap-daemon-site (Siddarth Seth, via Gopal V)
Signed-off-by: Gopal V <gopalv@apache.org>
(cherry picked from commit f8f50ab17da3155489e96cdf2e9b849aa88b8700)

Conflicts:
	common/src/java/org/apache/hadoop/hive/conf/HiveConf.java

common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -26,9 +26,11 @@ import java.io.InputStream;
 import java.io.PrintStream;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -40,6 +42,7 @@ import java.util.regex.Pattern;
 import javax.security.auth.login.LoginException;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hive.common.classification.InterfaceAudience;
 import org.apache.hadoop.hive.common.classification.InterfaceAudience.LimitedPrivate;
 import org.apache.hadoop.hive.conf.Validator.PatternSet;
 import org.apache.hadoop.hive.conf.Validator.RangeValidator;
@@ -108,7 +111,16 @@ public class HiveConf extends Configuration {
     for (ConfVars confVar : ConfVars.values()) {
       vars.put(confVar.varname, confVar);
     }
+
+    Set<String> llapDaemonConfVarsSetLocal = new LinkedHashSet<>();
+    populateLlapDaemonVarsSet(llapDaemonConfVarsSetLocal);
+    llapDaemonVarsSet = Collections.unmodifiableSet(llapDaemonConfVarsSetLocal);
   }
+
+  @InterfaceAudience.Private
+  public static final String PREFIX_LLAP = "llap.";
+  @InterfaceAudience.Private
+  public static final String PREFIX_HIVE_LLAP = "hive.llap.";
 
   /**
    * Metastore related options that the db is initialized against. When a conf
@@ -232,6 +244,70 @@ public class HiveConf extends Configuration {
     HiveConf.ConfVars.METASTOREWAREHOUSE,
     HiveConf.ConfVars.SCRATCHDIR
   };
+
+  /**
+   * Variables used by LLAP daemons.
+   * TODO: Eventually auto-populate this based on prefixes. The conf variables
+   * will need to be renamed for this.
+   */
+  private static final Set<String> llapDaemonVarsSet;
+
+  private static void populateLlapDaemonVarsSet(Set<String> llapDaemonVarsSetLocal) {
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_IO_ENABLED.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_IO_MEMORY_MODE.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_ALLOCATOR_MIN_ALLOC.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_ALLOCATOR_MAX_ALLOC.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_ALLOCATOR_ARENA_COUNT.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_IO_MEMORY_MAX_SIZE.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_ALLOCATOR_DIRECT.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_USE_LRFU.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_LRFU_LAMBDA.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_CACHE_ALLOW_SYNTHETIC_FILEID.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_IO_USE_FILEID_PATH.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_ORC_ENABLE_TIME_COUNTERS.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_IO_THREADPOOL_SIZE.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_KERBEROS_PRINCIPAL.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_KERBEROS_KEYTAB_FILE.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_ZKSM_KERBEROS_PRINCIPAL.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_ZKSM_KERBEROS_KEYTAB_FILE.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_ZKSM_ZK_CONNECTION_STRING.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_SECURITY_ACL.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_MANAGEMENT_ACL.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DELEGATION_TOKEN_LIFETIME.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_MANAGEMENT_RPC_PORT.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_WEB_AUTO_AUTH.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_RPC_NUM_HANDLERS.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_WORK_DIRS.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_YARN_SHUFFLE_PORT.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_YARN_CONTAINER_MB.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_SHUFFLE_DIR_WATCHER_ENABLED.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_AM_LIVENESS_HEARTBEAT_INTERVAL_MS.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_AM_LIVENESS_CONNECTION_TIMEOUT_MS.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_AM_LIVENESS_CONNECTION_SLEEP_BETWEEN_RETRIES_MS.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_NUM_EXECUTORS.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_RPC_PORT.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_MEMORY_PER_INSTANCE_MB.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_VCPUS_PER_INSTANCE.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_NUM_FILE_CLEANER_THREADS.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_FILE_CLEANUP_DELAY_SECONDS.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_SERVICE_HOSTS.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_SERVICE_REFRESH_INTERVAL.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_ALLOW_PERMANENT_FNS.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_TASK_SCHEDULER_WAIT_QUEUE_SIZE.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_WAIT_QUEUE_COMPARATOR_CLASS_NAME.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_TASK_SCHEDULER_ENABLE_PREEMPTION.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_WEB_PORT.varname);
+    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_WEB_SSL.varname);
+  }
+
+  /**
+   * Get a set containing configuration parameter names used by LLAP Server isntances
+   * @return an unmodifiable set containing llap ConfVars
+   */
+  public static final Set<String> getLlapDaemonConfVars() {
+    return llapDaemonVarsSet;
+  }
+
 
   /**
    * ConfVars.
@@ -2457,7 +2533,7 @@ public class HiveConf extends Configuration {
     LLAP_DAEMON_YARN_SHUFFLE_PORT("hive.llap.daemon.yarn.shuffle.port", 15551,
       "YARN shuffle port for LLAP-daemon-hosted shuffle.", "llap.daemon.yarn.shuffle.port"),
     LLAP_DAEMON_YARN_CONTAINER_MB("hive.llap.daemon.yarn.container.mb", -1,
-      "TODO doc. Unused?", "llap.daemon.yarn.container.mb"),
+      "llap server yarn container size in MB. Used in LlapServiceDriver and package.py", "llap.daemon.yarn.container.mb"),
     LLAP_DAEMON_SHUFFLE_DIR_WATCHER_ENABLED("hive.llap.daemon.shuffle.dir.watcher.enabled", false,
       "TODO doc", "llap.daemon.shuffle.dir-watcher.enabled"),
     LLAP_DAEMON_AM_LIVENESS_HEARTBEAT_INTERVAL_MS(

bin/ext/llap.sh
@@ -30,6 +30,7 @@ llap () {
 
   set -e;
 
+  export HADOOP_CLIENT_OPTS="$HADOOP_CLIENT_OPTS -Dlog4j.configurationFile=llap-cli-log4j2.properties "
   # hadoop 20 or newer - skip the aux_jars option. picked up from hiveconf
   $HADOOP $CLASS $HIVE_OPTS -directory $TMPDIR "$@"
   

llap-client/src/java/org/apache/hadoop/hive/llap/registry/impl/LlapFixedRegistryImpl.java
@@ -31,7 +31,6 @@ import org.apache.hadoop.classification.InterfaceAudience;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
-import org.apache.hadoop.hive.llap.configuration.LlapConfiguration;
 import org.apache.hadoop.hive.llap.registry.ServiceInstance;
 import org.apache.hadoop.hive.llap.registry.ServiceInstanceSet;
 import org.apache.hadoop.hive.llap.registry.ServiceRegistry;
@@ -67,9 +66,8 @@ public class LlapFixedRegistryImpl implements ServiceRegistry {
     this.mngPort = HiveConf.getIntVar(conf, ConfVars.LLAP_MANAGEMENT_RPC_PORT);
 
     for (Map.Entry<String, String> kv : conf) {
-      if (kv.getKey().startsWith(LlapConfiguration.LLAP_DAEMON_PREFIX)
-          || kv.getKey().startsWith("hive.llap.")
-          || kv.getKey().startsWith(LlapConfiguration.LLAP_PREFIX)) {
+      if (kv.getKey().startsWith(HiveConf.PREFIX_LLAP)
+          || kv.getKey().startsWith(HiveConf.PREFIX_HIVE_LLAP)) {
         // TODO: read this somewhere useful, like the task scheduler
         srv.put(kv.getKey(), kv.getValue());
       }

llap-client/src/java/org/apache/hadoop/hive/llap/registry/impl/LlapYarnRegistryImpl.java
@@ -39,7 +39,6 @@ import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
-import org.apache.hadoop.hive.llap.configuration.LlapConfiguration;
 import org.apache.hadoop.hive.llap.registry.ServiceInstance;
 import org.apache.hadoop.hive.llap.registry.ServiceInstanceSet;
 import org.apache.hadoop.hive.llap.registry.ServiceRegistry;
@@ -161,8 +160,8 @@ public class LlapYarnRegistryImpl implements ServiceRegistry {
     srv.addExternalEndpoint(getServicesEndpoint());
 
     for (Map.Entry<String, String> kv : this.conf) {
-      if (kv.getKey().startsWith(LlapConfiguration.LLAP_DAEMON_PREFIX)
-          || kv.getKey().startsWith("hive.llap.")) {
+      if (kv.getKey().startsWith(HiveConf.PREFIX_LLAP)
+          || kv.getKey().startsWith(HiveConf.PREFIX_HIVE_LLAP)) {
         // TODO: read this somewhere useful, like the task scheduler
         srv.set(kv.getKey(), kv.getValue());
       }

llap-server/src/java/org/apache/hadoop/hive/llap/cli/LlapOptionsProcessor.java
@@ -18,8 +18,9 @@
 
 package org.apache.hadoop.hive.llap.cli;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import javax.annotation.Nonnull;
@@ -36,6 +37,21 @@ import org.apache.hadoop.util.StringUtils;
 
 public class LlapOptionsProcessor {
 
+  public static final String OPTION_INSTANCES = "instances"; //forward as arg
+  public static final String OPTION_NAME = "name"; // forward as arg
+  public static final String OPTION_DIRECTORY = "directory"; // work-dir
+  public static final String OPTION_ARGS = "args"; // forward as arg
+  public static final String OPTION_LOGLEVEL = "loglevel"; // forward as arg
+  public static final String OPTION_CHAOS_MONKEY = "chaosmonkey"; // forward as arg
+  public static final String OPTION_EXECUTORS = "executors"; // llap-daemon-site
+  public static final String OPTION_CACHE = "cache"; // llap-daemon-site
+  public static final String OPTION_SIZE = "size"; // forward via config.json
+  public static final String OPTION_XMX = "xmx"; // forward as arg
+  public static final String OPTION_AUXJARS = "auxjars"; // used to localize jars
+  public static final String OPTION_AUXHBASE = "auxhbase"; // used to localize jars
+  public static final String OPTION_JAVA_HOME = "javaHome"; // forward via config.json
+  public static final String OPTION_HIVECONF = "hiveconf"; // llap-daemon-site if relevant parameter
+
   public class LlapOptions {
     private final int instances;
     private final String directory;
@@ -47,10 +63,12 @@ public class LlapOptionsProcessor {
     private final String jars;
     private final boolean isHbase;
     private final Properties conf;
+    private final String javaPath;
 
     public LlapOptions(String name, int instances, String directory, int executors, long cache,
-        long size, long xmx, String jars, boolean isHbase, @Nonnull Properties hiveconf)
-            throws ParseException {
+                       long size, long xmx, String jars, boolean isHbase,
+                       @Nonnull Properties hiveconf, String javaPath)
+        throws ParseException {
       if (instances <= 0) {
         throw new ParseException("Invalid configuration: " + instances
             + " (should be greater than 0)");
@@ -65,6 +83,7 @@ public class LlapOptionsProcessor {
       this.jars = jars;
       this.isHbase = isHbase;
       this.conf = hiveconf;
+      this.javaPath = javaPath;
     }
 
     public String getName() {
@@ -106,57 +125,67 @@ public class LlapOptionsProcessor {
     public Properties getConfig() {
       return conf;
     }
+
+    public String getJavaPath() {
+      return javaPath;
+    }
   }
 
   protected static final Logger l4j = LoggerFactory.getLogger(LlapOptionsProcessor.class.getName());
   private final Options options = new Options();
-  Map<String, String> hiveVariables = new HashMap<String, String>();
   private org.apache.commons.cli.CommandLine commandLine;
 
   @SuppressWarnings("static-access")
   public LlapOptionsProcessor() {
 
     // set the number of instances on which llap should run
-    options.addOption(OptionBuilder.hasArg().withArgName("instances").withLongOpt("instances")
+    options.addOption(OptionBuilder.hasArg().withArgName(OPTION_INSTANCES).withLongOpt(OPTION_INSTANCES)
         .withDescription("Specify the number of instances to run this on").create('i'));
 
-    options.addOption(OptionBuilder.hasArg().withArgName("name").withLongOpt("name")
+    options.addOption(OptionBuilder.hasArg().withArgName(OPTION_NAME).withLongOpt(OPTION_NAME)
         .withDescription("Cluster name for YARN registry").create('n'));
 
-    options.addOption(OptionBuilder.hasArg().withArgName("directory").withLongOpt("directory")
+    options.addOption(OptionBuilder.hasArg().withArgName(OPTION_DIRECTORY).withLongOpt(OPTION_DIRECTORY)
         .withDescription("Temp directory for jars etc.").create('d'));
 
-    options.addOption(OptionBuilder.hasArg().withArgName("args").withLongOpt("args")
+    options.addOption(OptionBuilder.hasArg().withArgName(OPTION_ARGS).withLongOpt(OPTION_ARGS)
         .withDescription("java arguments to the llap instance").create('a'));
 
-    options.addOption(OptionBuilder.hasArg().withArgName("loglevel").withLongOpt("loglevel")
+    options.addOption(OptionBuilder.hasArg().withArgName(OPTION_LOGLEVEL).withLongOpt(OPTION_LOGLEVEL)
         .withDescription("log levels for the llap instance").create('l'));
 
-    options.addOption(OptionBuilder.hasArg().withArgName("chaosmonkey").withLongOpt("chaosmonkey")
+    options.addOption(OptionBuilder.hasArg().withArgName(OPTION_CHAOS_MONKEY).withLongOpt(OPTION_CHAOS_MONKEY)
         .withDescription("chaosmonkey interval").create('m'));
 
-    options.addOption(OptionBuilder.hasArg().withArgName("executors").withLongOpt("executors")
+    options.addOption(OptionBuilder.hasArg().withArgName(OPTION_EXECUTORS).withLongOpt(OPTION_EXECUTORS)
         .withDescription("executor per instance").create('e'));
 
-    options.addOption(OptionBuilder.hasArg().withArgName("cache").withLongOpt("cache")
+    options.addOption(OptionBuilder.hasArg().withArgName(OPTION_CACHE).withLongOpt(OPTION_CACHE)
         .withDescription("cache size per instance").create('c'));
 
-    options.addOption(OptionBuilder.hasArg().withArgName("size").withLongOpt("size")
+    options.addOption(OptionBuilder.hasArg().withArgName(OPTION_SIZE).withLongOpt(OPTION_SIZE)
         .withDescription("container size per instance").create('s'));
 
-    options.addOption(OptionBuilder.hasArg().withArgName("xmx").withLongOpt("xmx")
+    options.addOption(OptionBuilder.hasArg().withArgName(OPTION_XMX).withLongOpt(OPTION_XMX)
         .withDescription("working memory size").create('w'));
 
-    options.addOption(OptionBuilder.hasArg().withArgName("auxjars").withLongOpt("auxjars")
+    options.addOption(OptionBuilder.hasArg().withArgName(OPTION_AUXJARS).withLongOpt(OPTION_AUXJARS)
         .withDescription("additional jars to package (by default, JSON SerDe jar is packaged"
             + " if available)").create('j'));
 
-    options.addOption(OptionBuilder.hasArg().withArgName("auxhbase").withLongOpt("auxhbase")
+    options.addOption(OptionBuilder.hasArg().withArgName(OPTION_AUXHBASE).withLongOpt(OPTION_AUXHBASE)
         .withDescription("whether to package the HBase jars (true by default)").create('h'));
+
+    options.addOption(OptionBuilder.hasArg().withArgName(OPTION_JAVA_HOME).withLongOpt(OPTION_JAVA_HOME)
+        .withDescription(
+            "Path to the JRE/JDK. This should be installed at the same location on all cluster nodes ($JAVA_HOME, java.home by default)")
+        .create());
 
     // -hiveconf x=y
     options.addOption(OptionBuilder.withValueSeparator().hasArgs(2).withArgName("property=value")
-        .withLongOpt("hiveconf").withDescription("Use value for given property").create());
+        .withLongOpt(OPTION_HIVECONF)
+        .withDescription("Use value for given property. Overridden by explicit parameters")
+        .create());
 
     // [-H|--help]
     options.addOption(new Option("H", "help", false, "Print help information"));
@@ -166,7 +195,7 @@ public class LlapOptionsProcessor {
     return StringUtils.TraditionalBinaryPrefix.string2long(value);
   }
 
-  public LlapOptions processOptions(String argv[]) throws ParseException {
+  public LlapOptions processOptions(String argv[]) throws ParseException, IOException {
     commandLine = new GnuParser().parse(options, argv);
     if (commandLine.hasOption('H') || false == commandLine.hasOption("instances")) {
       // needs at least --instances
@@ -174,31 +203,34 @@ public class LlapOptionsProcessor {
       return null;
     }
 
-    int instances = Integer.parseInt(commandLine.getOptionValue("instances"));
-    String directory = commandLine.getOptionValue("directory");
-    String jars = commandLine.getOptionValue("auxjars");
+    int instances = Integer.parseInt(commandLine.getOptionValue(OPTION_INSTANCES));
+    String directory = commandLine.getOptionValue(OPTION_DIRECTORY);
+    String jars = commandLine.getOptionValue(OPTION_AUXJARS);
 
-    String name = commandLine.getOptionValue("name", null);
+    String name = commandLine.getOptionValue(OPTION_NAME, null);
 
-    final int executors = Integer.parseInt(commandLine.getOptionValue("executors", "-1"));
-    final long cache = parseSuffixed(commandLine.getOptionValue("cache", "-1"));
-    final long size = parseSuffixed(commandLine.getOptionValue("size", "-1"));
-    final long xmx = parseSuffixed(commandLine.getOptionValue("xmx", "-1"));
-    final boolean isHbase = Boolean.parseBoolean(commandLine.getOptionValue("auxhbase", "true"));
+    final int executors = Integer.parseInt(commandLine.getOptionValue(OPTION_EXECUTORS, "-1"));
+    final long cache = parseSuffixed(commandLine.getOptionValue(OPTION_CACHE, "-1"));
+    final long size = parseSuffixed(commandLine.getOptionValue(OPTION_SIZE, "-1"));
+    final long xmx = parseSuffixed(commandLine.getOptionValue(OPTION_XMX, "-1"));
+    final boolean isHbase = Boolean.parseBoolean(commandLine.getOptionValue(OPTION_AUXHBASE, "true"));
 
     final Properties hiveconf;
 
-    if (commandLine.hasOption("hiveconf")) {
-      hiveconf = commandLine.getOptionProperties("hiveconf");
+    if (commandLine.hasOption(OPTION_HIVECONF)) {
+      hiveconf = commandLine.getOptionProperties(OPTION_HIVECONF);
     } else {
       hiveconf = new Properties();
     }
 
+    String javaHome = null;
+    if (commandLine.hasOption(OPTION_JAVA_HOME)) {
+      javaHome = commandLine.getOptionValue(OPTION_JAVA_HOME);
+    }
     // loglevel, chaosmonkey & args are parsed by the python processor
 
     return new LlapOptions(
-        name, instances, directory, executors, cache, size, xmx, jars, isHbase, hiveconf);
-
+        name, instances, directory, executors, cache, size, xmx, jars, isHbase, hiveconf, javaHome);
   }
 
   private void printUsage() {

commit 9c373bb39e330d76bf91046d2fcb76884880cc55

Fix build broken by ce8bf5445be00b6d9f95dffbb5affbd7daf42fa6 (Sergey Shelukhin)

common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -292,7 +292,6 @@ public class HiveConf extends Configuration {
     llapDaemonVarsSetLocal.add(ConfVars.LLAP_FILE_CLEANUP_DELAY_SECONDS.varname);
     llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_SERVICE_HOSTS.varname);
     llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_SERVICE_REFRESH_INTERVAL.varname);
-    llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_ALLOW_PERMANENT_FNS.varname);
     llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_TASK_SCHEDULER_WAIT_QUEUE_SIZE.varname);
     llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_WAIT_QUEUE_COMPARATOR_CLASS_NAME.varname);
     llapDaemonVarsSetLocal.add(ConfVars.LLAP_DAEMON_TASK_SCHEDULER_ENABLE_PREEMPTION.varname);

llap-server/src/java/org/apache/hadoop/hive/llap/daemon/impl/ContainerRunnerImpl.java
@@ -27,7 +27,6 @@ import java.util.concurrent.atomic.AtomicReference;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
-import org.apache.hadoop.hive.llap.configuration.LlapConfiguration;
 import org.apache.hadoop.hive.llap.daemon.ContainerRunner;
 import org.apache.hadoop.hive.llap.daemon.FragmentCompletionHandler;
 import org.apache.hadoop.hive.llap.daemon.HistoryLogger;

llap-tez/src/java/org/apache/hadoop/hive/llap/tezplugins/LlapTaskCommunicator.java
@@ -36,7 +36,6 @@ import org.apache.hadoop.fs.CommonConfigurationKeysPublic;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.conf.HiveConf.ConfVars;
 import org.apache.hadoop.hive.llap.LlapNodeId;
-import org.apache.hadoop.hive.llap.configuration.LlapConfiguration;
 import org.apache.hadoop.hive.llap.daemon.rpc.LlapDaemonProtocolProtos;
 import org.apache.hadoop.hive.llap.daemon.rpc.LlapDaemonProtocolProtos.FragmentRuntimeInfo;
 import org.apache.hadoop.hive.llap.daemon.rpc.LlapDaemonProtocolProtos.QueryCompleteRequestProto;