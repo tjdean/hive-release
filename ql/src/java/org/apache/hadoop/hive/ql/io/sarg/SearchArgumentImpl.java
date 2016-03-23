36ad10c10ad28fc8851f11ffab974b7575a3b1fa
Prasanth Jayachandran <prasanthj@apache.org>
2016-03-23 12:41:53 -0500

    BUG-51236:HIVE-12022 - (2.4) NPE: hive.ql.io.sarg.SearchArgumentImpl

--- ql/src/java/org/apache/hadoop/hive/ql/io/sarg/SearchArgumentImpl.java
+++ ql/src/java/org/apache/hadoop/hive/ql/io/sarg/SearchArgumentImpl.java
@@ -144,7 +144,7 @@ final class SearchArgumentImpl implements SearchArgument {
       } else if (literalList != null) {
         for(Object lit: literalList) {
           buffer.append(' ');
-          buffer.append(lit.toString());
+          buffer.append(lit == null ? "null" : lit.toString());
         }
       }
       buffer.append(')');
