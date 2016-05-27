597bf5f8aae5d9a54284d163db5fe4b6f7cd07b0
David Mogyorosi <david_mogyorosi@epam.com>
2016-05-27 16:01:31 +0200

    BUG-55811 HIVE-13242: DISTINCT keyword is dropped by the parser for windowing (Jesus Camacho Rodriguez, reviewed by Ashutosh Chauhan)

--- ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/reloperators/HiveAggregate.java
+++ ql/src/java/org/apache/hadoop/hive/ql/optimizer/calcite/reloperators/HiveAggregate.java
@@ -25,7 +25,6 @@ import org.apache.calcite.plan.RelOptCluster;
 import org.apache.calcite.plan.RelOptCost;
 import org.apache.calcite.plan.RelOptPlanner;
 import org.apache.calcite.plan.RelTraitSet;
-import org.apache.calcite.rel.InvalidRelException;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.Aggregate;
 import org.apache.calcite.rel.core.AggregateCall;
@@ -50,7 +49,7 @@ public class HiveAggregate extends Aggregate implements HiveRelNode {
 
   public HiveAggregate(RelOptCluster cluster, RelTraitSet traitSet, RelNode child,
       boolean indicator, ImmutableBitSet groupSet, List<ImmutableBitSet> groupSets,
-      List<AggregateCall> aggCalls) throws InvalidRelException {
+      List<AggregateCall> aggCalls) {
     super(cluster, TraitsUtil.getDefaultTraitSet(cluster), child, indicator, groupSet,
             groupSets, aggCalls);
   }
@@ -59,14 +58,8 @@ public class HiveAggregate extends Aggregate implements HiveRelNode {
   public Aggregate copy(RelTraitSet traitSet, RelNode input,
           boolean indicator, ImmutableBitSet groupSet,
           List<ImmutableBitSet> groupSets, List<AggregateCall> aggCalls) {
-    try {
       return new HiveAggregate(getCluster(), traitSet, input, indicator, groupSet,
               groupSets, aggCalls);
-    } catch (InvalidRelException e) {
-      // Semantic error not possible. Must be a bug. Convert to
-      // internal error.
-      throw new AssertionError(e);
-    }
   }
 
   @Override
@@ -145,12 +138,8 @@ public class HiveAggregate extends Aggregate implements HiveRelNode {
     public RelNode createAggregate(RelNode child, boolean indicator,
             ImmutableBitSet groupSet, ImmutableList<ImmutableBitSet> groupSets,
             List<AggregateCall> aggCalls) {
-      try {
         return new HiveAggregate(child.getCluster(), child.getTraitSet(), child, indicator,
                 groupSet, groupSets, aggCalls);
-      } catch (InvalidRelException e) {
-          throw new RuntimeException(e);
-      }
     }
   }
 }
