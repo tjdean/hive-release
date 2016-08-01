283ddd06c3c84fb009506920ae4e65abf7f7b308
Chinna Rao L <chinnaraol@apache.org>
2016-08-01 20:16:59 +0530

    BUG-65355 HIVE-12077 : MSCK Repair table should fix partitions in batches (Chinna Rao L , via Chinna Rao L)
    (cherry-picked: d20921100efb4411a82cb716e3e0471cbc118c71)
    
    Change-Id: If54b4276d10b25b63ec4cf0956bb3fe54c1ac318

--- common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
+++ common/src/java/org/apache/hadoop/hive/conf/HiveConf.java
@@ -2404,6 +2404,11 @@ public class HiveConf extends Configuration {
        "directories that are partition-like but contain unsupported characters. 'throw' (an " +
        "exception) is the default; 'skip' will skip the invalid directories and still repair the" +
        " others; 'ignore' will skip the validation (legacy behavior, causes bugs in many cases)"),
+    HIVE_MSCK_REPAIR_BATCH_SIZE(
+        "hive.msck.repair.batch.size", 0,
+        "Batch size for the msck repair command. If the value is greater than zero, "
+            + "it will execute batch wise with the configured batch size. "
+            + "The default value is zero. Zero means it will execute directly (Not batch wise)"),
     HIVE_TEZ_ENABLE_MEMORY_MANAGER("hive.tez.enable.memory.manager", true,
         "Enable memory manager for tez"),
     HIVE_HASH_TABLE_INFLATION_FACTOR("hive.hash.table.inflation.factor", (float) 2.0,
