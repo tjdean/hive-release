22c824c4f40ae1105392a8872a53bff19f3e9ace
Attila Szucs <aszucs@hortonworks.com>
2016-08-17 14:40:36 +0200

    BUG-68438 HIVE-13083 Writing HiveDecimal to ORC can wrongly suppress present stream
    (cherry picked from commit dc28cd7158883cc577bb77c6d63a065b805f94b0)
    
    Change-Id: I2f45ede043a472635bf0d4820fc89614a27affbe

--- ql/src/java/org/apache/hadoop/hive/ql/io/orc/WriterImpl.java
+++ ql/src/java/org/apache/hadoop/hive/ql/io/orc/WriterImpl.java
@@ -1678,13 +1678,15 @@ public class WriterImpl implements Writer, MemoryManager.Callback {
 
     @Override
     void write(Object obj) throws IOException {
+      HiveDecimal decimal = null;
+      if (obj != null) {
+        decimal = ((HiveDecimalObjectInspector) inspector).getPrimitiveJavaObject(obj);
+        if (decimal == null) {
+          obj = null;
+        }
+      }
       super.write(obj);
       if (obj != null) {
-        HiveDecimal decimal = ((HiveDecimalObjectInspector) inspector).
-            getPrimitiveJavaObject(obj);
-        if (decimal == null) {
-          return;
-        }
         SerializationUtils.writeBigInteger(valueStream,
             decimal.unscaledValue());
         scaleStream.write(decimal.scale());
@@ -2375,7 +2377,7 @@ public class WriterImpl implements Writer, MemoryManager.Callback {
         .setMagic(OrcFile.MAGIC)
         .addVersion(version.getMajor())
         .addVersion(version.getMinor())
-        .setWriterVersion(OrcFile.WriterVersion.HIVE_8732.getId());
+        .setWriterVersion(OrcFile.WriterVersion.HIVE_13083.getId());
     if (compress != CompressionKind.NONE) {
       builder.setCompressionBlockSize(bufferSize);
     }
