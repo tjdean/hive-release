626f055b815fa1ccc9ffbcf40718803ebd99efe9
Rajesh Balamohan <rbalamohan@apache.org>
2016-09-19 04:32:45 +0530

    BUG-61422: StatsUtils:: getFileSizeForPartitions could be expensive in queries with 'hive.stats.fetch.partition.stats=false'
    Change-Id: I20fdecf09a626d4ea114da51cc9510cfd22eb21c

--- ql/src/java/org/apache/hadoop/hive/ql/stats/StatsUtils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/stats/StatsUtils.java
@@ -22,6 +22,7 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import com.google.common.math.LongMath;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
@@ -88,6 +89,7 @@ import org.apache.hadoop.hive.serde2.objectinspector.primitive.WritableTimestamp
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.tez.mapreduce.hadoop.MRJobConfig;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayList;
@@ -96,6 +98,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class StatsUtils {
 
@@ -570,18 +577,42 @@ public class StatsUtils {
    *          - partition list
    * @return sizes of patitions
    */
-  public static List<Long> getFileSizeForPartitions(HiveConf conf, List<Partition> parts) {
-    List<Long> sizes = Lists.newArrayList();
-    for (Partition part : parts) {
-      Path path = part.getDataLocation();
-      long size = 0;
-      try {
-        FileSystem fs = path.getFileSystem(conf);
-        size = fs.getContentSummary(path).getLength();
-      } catch (Exception e) {
-        size = 0;
+  public static List<Long> getFileSizeForPartitions(final HiveConf conf, List<Partition> parts) {
+    LOG.info("Number of partitions : " + parts.size());
+    ArrayList<Future<Long>> futures = new ArrayList<>();
+
+    int threads = Math.max(1, conf.getIntVar(ConfVars.METASTORE_FS_HANDLER_THREADS_COUNT));
+    final ExecutorService pool = Executors.newFixedThreadPool(threads,
+                new ThreadFactoryBuilder()
+                    .setDaemon(true)
+                    .setNameFormat("Get-Partitions-Size-%d")
+                    .build());
+
+    final ArrayList<Long> sizes = new ArrayList<>(parts.size());
+    for (final Partition part : parts) {
+      final Path path = part.getDataLocation();
+      futures.add(pool.submit(new Callable<Long>() {
+        @Override
+        public Long call() throws Exception {
+          try {
+            LOG.debug("Partition path : " + path);
+            FileSystem fs = path.getFileSystem(conf);
+            return fs.getContentSummary(path).getLength();
+          } catch (IOException e) {
+            return 0L;
+          }
+        }
+      }));
+    }
+
+    try {
+      for(int i = 0; i < futures.size(); i++) {
+        sizes.add(i, futures.get(i).get());
       }
-      sizes.add(size);
+    } catch (InterruptedException | ExecutionException e) {
+      LOG.warn("Exception in processing files ", e);
+    } finally {
+      pool.shutdownNow();
     }
     return sizes;
   }
