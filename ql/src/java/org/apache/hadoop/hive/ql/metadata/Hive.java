c0b736000856cb8ae4247d32b82793ed241d8fb1
Prasanth Jayachandran <prasanthj@apache.org>
2016-09-22 10:46:09 -0700

    HIVE-908: HIVE-14814: Hive2: Hive.loadDynamicPartitions (transactional )fails with Nullpointer Exception at hive.ql.exec.MoveTask. nul
    Change-Id: I8b63d31f304815c88c6df087597b8a6e9145e9b9

--- ql/src/java/org/apache/hadoop/hive/ql/metadata/Hive.java
+++ ql/src/java/org/apache/hadoop/hive/ql/metadata/Hive.java
@@ -1657,7 +1657,7 @@ private void constructOneLBLocationMap(FileStatus fSta,
         for (Partition p : partitionsMap.values()) {
           partNames.add(p.getName());
         }
-        metaStoreClient.addDynamicPartitions(txnId, tbl.getDbName(), tbl.getTableName(),
+        getMSC().addDynamicPartitions(txnId, tbl.getDbName(), tbl.getTableName(),
           partNames, AcidUtils.toDataOperationType(operation));
       }
       return partitionsMap;
