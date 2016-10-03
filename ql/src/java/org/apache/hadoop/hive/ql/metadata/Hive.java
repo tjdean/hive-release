difin/difin_hive

HIVE-13857: insert overwrite select from some table fails throwing org.apache.hadoop.security.AccessControlException - II(Hari Subramaniyan, reviewed by Ashutosh Chauhan)

ql/src/java/org/apache/hadoop/hive/ql/metadata/Hive.java
@@ -2688,6 +2688,7 @@ private void constructOneLBLocationMap(FileStatus fSta,
             }
 
             Path destPath = new Path(destf, srcP.getName());
+            String srcGroup = srcFile.getGroup();
             if (!needToCopy && !isSrcLocal) {
               for (int counter = 1; !destFs.rename(srcP,destPath); counter++) {
                 destPath = new Path(destf, name + ("_copy_" + counter) + filetype);
@@ -2697,7 +2698,7 @@ private void constructOneLBLocationMap(FileStatus fSta,
             }
 
             if (inheritPerms) {
-              HdfsUtils.setFullFileStatus(conf, fullDestStatus, destFs, destPath, false);
+              HdfsUtils.setFullFileStatus(conf, fullDestStatus, srcGroup, destFs, destPath, false);
             }
             if (null != newFiles) {
               newFiles.add(destPath);
@@ -2871,10 +2872,11 @@ private void constructOneLBLocationMap(FileStatus fSta,
                 public Void call() throws Exception {
                   SessionState.setCurrentSessionState(parentSession);
                   Path destPath = new Path(destf, status.getPath().getName());
+                  String group = status.getGroup();
                   try {
                     if(destFs.rename(status.getPath(), destf)) {
                       if (inheritPerms) {
-                        HdfsUtils.setFullFileStatus(conf, desiredStatus, destFs, destPath, false);
+                        HdfsUtils.setFullFileStatus(conf, desiredStatus, group, destFs, destPath, false);
                       }
                     } else {
                       throw new IOException("rename for src path: " + status.getPath() + " to dest path:"

tjdean/hive-release

Conflicts:
	ql/src/java/org/apache/hadoop/hive/ql/exec/DDLTask.java
	ql/src/java/org/apache/hadoop/hive/ql/metadata/Hive.java
	shims/common/src/main/java/org/apache/hadoop/hive/io/HdfsUtils.java

ql/src/java/org/apache/hadoop/hive/ql/metadata/Hive.java
@@ -2544,6 +2544,7 @@ private void constructOneLBLocationMap(FileStatus fSta,
             }
 
             Path destPath = new Path(destf, srcP.getName());
+            String srcGroup = srcFile.getGroup();
             if (!needToCopy && !isSrcLocal) {
               for (int counter = 1; !destFs.rename(srcP,destPath); counter++) {
                 destPath = new Path(destf, name + ("_copy_" + counter) + filetype);
@@ -2553,7 +2554,7 @@ private void constructOneLBLocationMap(FileStatus fSta,
             }
 
             if (inheritPerms) {
-              ShimLoader.getHadoopShims().setFullFileStatus(conf, fullDestStatus, destFs, destPath, false);
+              ShimLoader.getHadoopShims().setFullFileStatus(conf, fullDestStatus, srcGroup, destFs, destPath, false);
             }
             if (null != newFiles) {
               newFiles.add(destPath);
@@ -2739,10 +2740,11 @@ private void constructOneLBLocationMap(FileStatus fSta,
                 public Void call() throws Exception {
                   SessionState.setCurrentSessionState(parentSession);
                   Path destPath = new Path(destf, status.getPath().getName());
+                  String group = status.getGroup();
                   try {
                     if(destFs.rename(status.getPath(), destf)) {
                       if (inheritPerms) {
-                        shims.setFullFileStatus(conf, desiredStatus, destFs, destPath, false);
+                        shims.setFullFileStatus(conf, desiredStatus, group, destFs, destPath, false);
                       }
                     } else {
                       throw new IOException("rename for src path: " + status.getPath() + " to dest path:"