e627096e9cdff59f4be4ca0b8b89e24c02a1b1c7
Daniel Dai <daijy@hortonworks.com>
2017-02-11 13:28:24 -0800

    BUG-72703: HIVE-15840: Webhcat test TestPig_5 failing with Pig on Tez at check for percent complete of job (rollback unintended changes)
    Change-Id: I9c7fbc1801711f9e91d8459715d810cc84397b27

--- hcatalog/webhcat/svr/src/main/java/org/apache/hive/hcatalog/templeton/tool/TempletonUtils.java
+++ hcatalog/webhcat/svr/src/main/java/org/apache/hive/hcatalog/templeton/tool/TempletonUtils.java
@@ -48,6 +48,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.shims.ShimLoader;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.util.Shell;
 import org.apache.hadoop.util.StringUtils;
