25c9b6a592b28690d05615d44f216040ce5cefec
Prasanth Jayachandran <prasanthj@apache.org>
2017-07-20 18:36:43 -0700

    BUG-84233: Backport HIVE-17085: ORC file merge/concatenation should do full schema check
    Change-Id: I9da18475eeb758308936d6203301cd481ac2bf05

--- ql/src/java/org/apache/hadoop/hive/ql/io/orc/OrcFileStripeMergeRecordReader.java
+++ ql/src/java/org/apache/hadoop/hive/ql/io/orc/OrcFileStripeMergeRecordReader.java
@@ -100,9 +100,10 @@ public class OrcFileStripeMergeRecordReader implements
         keyWrapper.setInputPath(path);
         keyWrapper.setCompression(reader.getCompression());
         keyWrapper.setCompressBufferSize(reader.getCompressionSize());
-        keyWrapper.setVersion(reader.getFileVersion());
+        keyWrapper.setFileVersion(reader.getFileVersion());
+        keyWrapper.setWriterVersion(reader.getWriterVersion());
         keyWrapper.setRowIndexStride(reader.getRowIndexStride());
-        keyWrapper.setTypes(reader.getTypes());
+        keyWrapper.setFileSchema(reader.getSchema());
       } else {
         stripeIdx++;
         continue;
