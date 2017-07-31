87791f30040ced07ec682eca2a24dac6ceea8b76
Thejas M Nair <thejas@hortonworks.com>
2017-07-30 23:12:00 -0700

    Revert "EAR-5998 - HIVE-16965: SMB join may produce incorrect results (Deepak Jaiswal, reviewed by gopalv)"
    This reverts commit 8b464b8bf744094c328f72e67855fc6ee225c657.

--- ql/src/java/org/apache/hadoop/hive/ql/exec/tez/MapRecordSource.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/tez/MapRecordSource.java
@@ -23,7 +23,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hive.ql.exec.MapOperator;
 import org.apache.hadoop.hive.ql.exec.mr.ExecMapperContext;
-import org.apache.hadoop.hive.ql.exec.tez.tools.KeyValueInputMerger;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapred.JobConf;
@@ -46,10 +45,6 @@ public class MapRecordSource implements RecordSource {
   void init(JobConf jconf, MapOperator mapOp, KeyValueReader reader) throws IOException {
     execContext = mapOp.getExecContext();
     this.mapOp = mapOp;
-    if (reader instanceof KeyValueInputMerger) {
-      KeyValueInputMerger kvMerger = (KeyValueInputMerger) reader;
-      kvMerger.setIOCxt(execContext.getIoCxt());
-    }
     this.reader = reader;
   }
 
