58f4c22dd058cf6d92e42b657264cf82b88d9c41
Anishek Agarwal <anishek@gmail.com>
2018-01-12 10:13:56 +0530

    BUG-94520 / HIVE-18352: introduce a METADATAONLY option while doing REPL DUMP to allow integrations of other tools (Anishek Agarwal reviewed by Daniel Dai)
    Change-Id: Ic6bc8d2623d1a8cd6afb44a9b50aa3143c5f4e80

--- itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/parse/TestReplicationScenariosAcrossInstances.java
+++ itests/hive-unit/src/test/java/org/apache/hadoop/hive/ql/parse/TestReplicationScenariosAcrossInstances.java
@@ -1,37 +1,34 @@
 /*
-  Licensed to the Apache Software Foundation (ASF) under one
-  or more contributor license agreements.  See the NOTICE file
-  distributed with this work for additional information
-  regarding copyright ownership.  The ASF licenses this file
-  to you under the Apache License, Version 2.0 (the
-  "License"); you may not use this file except in compliance
-  with the License.  You may obtain a copy of the License at
-  <p>
-  http://www.apache.org/licenses/LICENSE-2.0
-  <p>
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.hadoop.hive.ql.parse;
 
-import com.google.common.base.Function;
-import com.google.common.collect.Lists;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.PathFilter;
 import org.apache.hadoop.hdfs.MiniDFSCluster;
 import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.ql.parse.repl.PathBuilder;
 import org.apache.hadoop.hive.ql.session.DependencyResolver;
+import org.apache.hadoop.hive.shims.Utils;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
@@ -39,13 +36,14 @@ import org.junit.rules.TestRule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.Nullable;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
@@ -62,12 +60,15 @@ public class TestReplicationScenariosAcrossInstances {
   protected static final Logger LOG = LoggerFactory.getLogger(TestReplicationScenarios.class);
 
   private static WarehouseInstance primary, replica;
+  private static MiniDFSCluster miniDFSCluster;
 
   @BeforeClass
   public static void classLevelSetup() throws Exception {
     Configuration conf = new Configuration();
     conf.set("dfs.client.use.datanode.hostname", "true");
-    MiniDFSCluster miniDFSCluster =
+
+    conf.set("hadoop.proxyuser." + Utils.getUGI().getShortUserName() + ".hosts", "*");
+    miniDFSCluster =
         new MiniDFSCluster.Builder(conf).numDataNodes(1).format(true).build();
     primary = new WarehouseInstance(LOG, miniDFSCluster);
     replica = new WarehouseInstance(LOG, miniDFSCluster);
@@ -83,12 +84,13 @@ public class TestReplicationScenariosAcrossInstances {
 
   @Before
   public void setup() throws Throwable {
-    replV1BackwardCompat = primary.getReplivationV1CompatRule(new ArrayList<String>());
+    replV1BackwardCompat = primary.getReplivationV1CompatRule(new ArrayList<>());
     primaryDbName = testName.getMethodName() + "_" + +System.currentTimeMillis();
     replicatedDbName = "replicated_" + primaryDbName;
     primary.run("create database " + primaryDbName);
   }
 
+  @Test
   public void testCreateFunctionIncrementalReplication() throws Throwable {
     WarehouseInstance.Tuple bootStrapDump = primary.dump(primaryDbName, null);
     replica.load(replicatedDbName, bootStrapDump.dumpLocation)
@@ -109,13 +111,12 @@ public class TestReplicationScenariosAcrossInstances {
 
     // Test the idempotent behavior of CREATE FUNCTION
     replica.load(replicatedDbName, incrementalDump.dumpLocation)
-            .run("REPL STATUS " + replicatedDbName)
+        .run("REPL STATUS " + replicatedDbName)
         .verifyResult(incrementalDump.lastReplicationId)
-            .run("SHOW FUNCTIONS LIKE '" + replicatedDbName + "*'")
+        .run("SHOW FUNCTIONS LIKE '" + replicatedDbName + "*'")
         .verifyResult(replicatedDbName + ".testFunction");
   }
 
-  @Ignore("This testcase is commented as it uses UDF library on java 1.8 which is not supported. Another JIRA BUG-80469 will track this.")
   @Test
   public void testDropFunctionIncrementalReplication() throws Throwable {
     primary.run("CREATE FUNCTION " + primaryDbName
@@ -174,23 +175,14 @@ public class TestReplicationScenariosAcrossInstances {
     FileStatus[] fileStatuses = replica.miniDFSCluster.getFileSystem().globStatus(
         new Path(
             replica.functionsRoot + "/" + replicatedDbName.toLowerCase() + "/anotherfunction/*/*")
-        , new PathFilter() {
-          @Override
-          public boolean accept(Path path) {
-            return path.toString().endsWith("jar");
-          }
-        });
+        , path -> path.toString().endsWith("jar"));
     List<String> expectedDependenciesNames = dependencies.jarNames();
     assertThat(fileStatuses.length, is(equalTo(expectedDependenciesNames.size())));
     List<String> jars =
-        Lists.transform(Arrays.asList(fileStatuses), new Function<FileStatus, String>() {
-          @Nullable
-          @Override
-          public String apply(@Nullable FileStatus fileStatus) {
-            String[] splits = fileStatus.getPath().toString().split("/");
-            return splits[splits.length - 1];
-          }
-        });
+        Arrays.stream(fileStatuses).map(fileStatus -> {
+          String[] splits = fileStatus.getPath().toString().split("/");
+          return splits[splits.length - 1];
+        }).collect(Collectors.toList());
     assertThat(jars, containsInAnyOrder(expectedDependenciesNames.toArray()));
   }
 
@@ -202,24 +194,17 @@ public class TestReplicationScenariosAcrossInstances {
     }
 
     private String toJarSubSql() {
-      List<String> result = Lists.transform(fullQualifiedJarPaths, new Function<Path, String>() {
-        @Override
-        public String apply(@Nullable Path path) {
-          return "jar '" + path + "'";
-        }
-      });
-
+      List<String> result = fullQualifiedJarPaths.stream().map(path -> "jar '" + path + "'")
+          .collect(Collectors.toList());
       return StringUtils.join(result, ",");
     }
 
     private List<String> jarNames() {
-      return Lists.transform(fullQualifiedJarPaths, new Function<Path, String>() {
-        @Override
-        public String apply(@Nullable Path path) {
-          String[] splits = path.toString().split("/");
-          return splits[splits.length - 1];
-        }
-      });
+      return fullQualifiedJarPaths.stream().map(
+          path -> {
+            String[] splits = path.toString().split("/");
+            return splits[splits.length - 1];
+          }).collect(Collectors.toList());
     }
   }
 
@@ -227,18 +212,15 @@ public class TestReplicationScenariosAcrossInstances {
       throws IOException, URISyntaxException, SemanticException {
     List<URI> localUris = new DependencyResolver().downloadDependencies(new URI(ivyPath));
     List<Path> remotePaths = onWarehouse.copyToHDFS(localUris);
-    List<Path> collect = Lists.transform(remotePaths, new Function<Path, Path>() {
-      @Override
-      public Path apply(@Nullable Path path) {
-        try {
-          return PathBuilder
-              .fullyQualifiedHDFSUri(path, onWarehouse.miniDFSCluster.getFileSystem());
+    List<Path> collect = remotePaths.stream().map(path -> {
+      try {
+        return PathBuilder
+            .fullyQualifiedHDFSUri(path, onWarehouse.miniDFSCluster.getFileSystem());
 
-        } catch (Exception e) {
-          throw new RuntimeException(e);
-        }
+      } catch (Exception e) {
+        throw new RuntimeException(e);
       }
-    });
+    }).collect(Collectors.toList());
     return new Dependencies(collect);
   }
 
@@ -269,5 +251,130 @@ public class TestReplicationScenariosAcrossInstances {
         .verifyResult(tuple.lastReplicationId)
         .run("select country from t2 order by country")
         .verifyResults(new String[] { "france", "india", "us" });
+  }
+
+  @Test
+  public void parallelExecutionOfReplicationBootStrapLoad() throws Throwable {
+    WarehouseInstance.Tuple tuple = primary
+        .run("use " + primaryDbName)
+        .run("create table t1 (id int)")
+        .run("create table t2 (place string) partitioned by (country string)")
+        .run("insert into table t2 partition(country='india') values ('bangalore')")
+        .run("insert into table t2 partition(country='australia') values ('sydney')")
+        .run("insert into table t2 partition(country='russia') values ('moscow')")
+        .run("insert into table t2 partition(country='uk') values ('london')")
+        .run("insert into table t2 partition(country='us') values ('sfo')")
+        .run("insert into table t2 partition(country='france') values ('paris')")
+        .run("insert into table t2 partition(country='japan') values ('tokyo')")
+        .run("insert into table t2 partition(country='china') values ('hkg')")
+        .run("create table t3 (rank int)")
+        .dump(primaryDbName, null);
+
+    replica.hiveConf.setBoolVar(HiveConf.ConfVars.EXECPARALLEL, true);
+    replica.load(replicatedDbName, tuple.dumpLocation)
+        .run("use " + replicatedDbName)
+        .run("repl status " + replicatedDbName)
+        .verifyResult(tuple.lastReplicationId)
+        .run("show tables")
+        .verifyResults(new String[] { "t1", "t2", "t3" })
+        .run("select country from t2")
+        .verifyResults(Arrays.asList("india", "australia", "russia", "uk", "us", "france", "japan",
+            "china"));
+  }
+
+  @Test
+  public void testMetadataBootstrapDump() throws Throwable {
+    WarehouseInstance.Tuple tuple = primary.run("use " + primaryDbName)
+        .run("create table  acid_table (key int, value int) partitioned by (load_date date) " +
+            "clustered by(key) into 2 buckets stored as orc tblproperties ('transactional'='true')")
+        .run("create table table1 (i int, j int)")
+        .run("insert into table1 values (1,2)")
+        .dump(primaryDbName, null, Arrays.asList("'hive.repl.dump.metadata.only'='true'",
+            "'hive.repl.dump.include.acid.tables'='true'"));
+
+    replica.load(replicatedDbName, tuple.dumpLocation)
+        .run("use " + replicatedDbName)
+        .run("show tables")
+        .verifyResults(new String[] { "acid_table", "table1" })
+        .run("select * from table1")
+        .verifyResults(Collections.emptyList());
+  }
+
+  @Test
+  public void testIncrementalMetadataReplication() throws Throwable {
+    ////////////  Bootstrap   ////////////
+    WarehouseInstance.Tuple bootstrapTuple = primary
+        .run("use " + primaryDbName)
+        .run("create table table1 (i int, j int)")
+        .run("create table table2 (a int, city string) partitioned by (country string)")
+        .run("create table table3 (i int, j int)")
+        .run("insert into table1 values (1,2)")
+        .dump(primaryDbName, null, Arrays.asList("'hive.repl.dump.metadata.only'='true'",
+            "'hive.repl.dump.include.acid.tables'='true'"));
+
+    replica.load(replicatedDbName, bootstrapTuple.dumpLocation)
+        .run("use " + replicatedDbName)
+        .run("show tables")
+        .verifyResults(new String[] { "table1", "table2", "table3" })
+        .run("select * from table1")
+        .verifyResults(Collections.emptyList());
+
+    ////////////  First Incremental ////////////
+    WarehouseInstance.Tuple incrementalOneTuple =
+        primary
+            .run("use " + primaryDbName)
+            .run("alter table table1 rename to renamed_table1")
+            .run("insert into table2 partition(country='india') values (1,'mumbai') ")
+            .run("create table table4 (i int, j int)")
+            .dump(
+                "repl dump " + primaryDbName + " from " + bootstrapTuple.lastReplicationId + " to "
+                    + (Long.parseLong(bootstrapTuple.lastReplicationId) + 100L) + " limit 100 "
+                    + "with ('hive.repl.dump.metadata.only'='true')"
+            );
+
+    replica.load(replicatedDbName, incrementalOneTuple.dumpLocation)
+        .run("use " + replicatedDbName)
+        .run("show tables")
+        .verifyResults(new String[] { "renamed_table1", "table2", "table3", "table4" })
+        .run("select * from renamed_table1")
+        .verifyResults(Collections.emptyList())
+        .run("select * from table2")
+        .verifyResults(Collections.emptyList());
+
+    ////////////  Second Incremental ////////////
+    WarehouseInstance.Tuple secondIncremental = primary
+        .run("alter table table2 add columns (zipcode int)")
+        .run("alter table table3 change i a string")
+        .run("alter table table3 set tblproperties('custom.property'='custom.value')")
+        .run("drop table renamed_table1")
+        .dump("repl dump " + primaryDbName + " from " + incrementalOneTuple.lastReplicationId
+            + " with ('hive.repl.dump.metadata.only'='true')"
+        );
+
+    replica.load(replicatedDbName, secondIncremental.dumpLocation)
+        .run("use " + replicatedDbName)
+        .run("show tables")
+        .verifyResults(new String[] { "table2", "table3", "table4" })
+        .run("desc table3")
+        .verifyResults(new String[] {
+            "a                   \tstring              \t                    ",
+            "j                   \tint                 \t                    "
+        })
+        .run("desc table2")
+        .verifyResults(new String[] {
+            "a                   \tint                 \t                    ",
+            "city                \tstring              \t                    ",
+            "country             \tstring              \t                    ",
+            "zipcode             \tint                 \t                    ",
+            "\t \t ",
+            "# Partition Information\t \t ",
+            "# col_name            \tdata_type           \tcomment             ",
+            "\t \t ",
+            "country             \tstring              \t                    ",
+        })
+        .run("show tblproperties table3('custom.property')")
+        .verifyResults(new String[] {
+            "custom.value\t "
+        });
   }
 }
