48a8d2f211119df10bf0ffc043e0a6fdf40a1d6f
Laszlo Bodor <bodorlaszlo0202@gmail.com>
2018-08-01 13:21:00 +0200

    BUG-108329 HIVE-17840: HiveMetaStore eats exception if transactionalListeners.notifyEvent fail

--- metastore/src/test/org/apache/hadoop/hive/metastore/TestObjectStore.java
+++ metastore/src/test/org/apache/hadoop/hive/metastore/TestObjectStore.java
@@ -23,6 +23,7 @@ import org.apache.hadoop.hive.metastore.api.CurrentNotificationEventId;
 import org.apache.hadoop.hive.metastore.api.NotificationEvent;
 import org.apache.hadoop.hive.metastore.api.NotificationEventRequest;
 import org.apache.hadoop.hive.metastore.api.NotificationEventResponse;
+import org.apache.hadoop.hive.metastore.api.MetaException;
 import org.apache.hadoop.hive.metastore.messaging.EventMessage;
 import org.apache.hadoop.hive.metastore.model.MNotificationLog;
 import org.apache.hadoop.hive.metastore.model.MNotificationNextId;
@@ -64,7 +65,7 @@ public class TestObjectStore {
    * Test notification operations
    */
   @Test
-  public void testNotificationOps() throws InterruptedException {
+  public void testNotificationOps() throws InterruptedException, MetaException {
     final int NO_EVENT_ID = 0;
     final int FIRST_EVENT_ID = 1;
     final int SECOND_EVENT_ID = 2;
@@ -116,7 +117,7 @@ public class TestObjectStore {
           + " https://db.apache.org/derby/docs/10.10/devguide/cdevconcepts842385.html"
   )
   @Test
-  public void testConcurrentAddNotifications() throws ExecutionException, InterruptedException {
+  public void testConcurrentAddNotifications() throws ExecutionException, InterruptedException, MetaException {
 
     final int NUM_THREADS = 10;
     CyclicBarrier cyclicBarrier = new CyclicBarrier(NUM_THREADS,
@@ -166,10 +167,10 @@ public class TestObjectStore {
 
             try {
               cyclicBarrier.await();
-            } catch (InterruptedException | BrokenBarrierException e) {
+              store.addNotificationEvent(dbEvent);
+            } catch (InterruptedException | BrokenBarrierException | MetaException e) {
               throw new RuntimeException(e);
             }
-            store.addNotificationEvent(dbEvent);
             System.out.println("FINISH NOTIFICATION");
           });
     }
