1cee4916d081b79c6da11e3d3af53aa4454b9dc3
David Lavati <david.lavati@gmail.com>
2019-03-07 13:01:04 +0100

    BUG-118630 HIVE-21404: MSSQL upgrade script alters the wrong column
    Change-Id: I0ec7ff56ca0dc4eb3e0c097fa5ebf7f0234170ab

--- standalone-metastore/src/main/sql/mssql/upgrade-3.0.0-to-3.1.0.mssql.sql
+++ standalone-metastore/src/main/sql/mssql/upgrade-3.0.0-to-3.1.0.mssql.sql
@@ -69,6 +69,47 @@ CREATE TABLE TXN_WRITE_NOTIFICATION_LOG (
 ALTER TABLE TXN_WRITE_NOTIFICATION_LOG ADD CONSTRAINT TXN_WRITE_NOTIFICATION_LOG_PK PRIMARY KEY (WNL_TXNID, WNL_DATABASE, WNL_TABLE, WNL_PARTITION);
 INSERT INTO SEQUENCE_TABLE (SEQUENCE_NAME, NEXT_VAL) VALUES ('org.apache.hadoop.hive.metastore.model.MTxnWriteNotificationLog', 1);
 
+-- We can not change the datatype of a column with default value. Hence we first drop the default constraint
+-- and then change the datatype. We wrap the code to drop the default constraint in a stored procedure to avoid
+-- code duplicate. We create temporary stored procedures since we do not need them during normal
+-- metastore operation.
+CREATE PROCEDURE #DROP_DEFAULT_CONSTRAINT @TBL_NAME sysname, @COL_NAME sysname
+AS
+BEGIN
+	DECLARE @constraintname sysname
+	SELECT @constraintname = default_constraints.name
+		FROM sys.all_columns INNER JOIN sys.tables ON all_columns.object_id = tables.object_id
+			INNER JOIN sys.schemas ON tables.schema_id = schemas.schema_id
+			INNER JOIN sys.default_constraints ON all_columns.default_object_id = default_constraints.object_id
+		WHERE schemas.name = 'dbo' AND tables.name = @TBL_NAME AND all_columns.name = @COL_NAME
+
+	IF (@constraintname IS NOT NULL)
+	BEGIN
+		DECLARE @sql nvarchar(max) = 'ALTER TABLE [dbo].' + QUOTENAME(@TBL_NAME) + ' DROP CONSTRAINT ' + QUOTENAME(@constraintname)
+		EXEC(@sql)
+	END
+END;
+
+-- Similarly for primary key constraint
+CREATE PROCEDURE #DROP_PRIMARY_KEY_CONSTRAINT @TBL_NAME sysname
+AS
+BEGIN
+	DECLARE @constraintname sysname
+	SELECT @constraintname = constraint_name
+		FROM information_schema.table_constraints
+		WHERE constraint_type = 'PRIMARY KEY' AND table_schema = 'dbo' AND table_name = @TBL_NAME
+	IF @constraintname IS NOT NULL
+	BEGIN
+	    DECLARE @sql_pk nvarchar(max) = 'ALTER TABLE [dbo].' + QUOTENAME(@TBL_NAME) + ' DROP CONSTRAINT ' + @constraintname
+	    EXEC(@sql_pk)
+	end
+END;
+
+-- HIVE-20221
+-- relying on the below SP in upgrade-2.1.0-to-2.2.0.mssql.sql already being created in this session (see HIVE-21462)
+EXEC #DROP_DEFAULT_CONSTRAINT "PARTITION_PARAMS", "PARAM_VALUE";
+ALTER TABLE "PARTITION_PARAMS" ALTER COLUMN "PARAM_VALUE" varchar(max);
+
 -- These lines need to be last.  Insert any changes above.
 UPDATE VERSION SET SCHEMA_VERSION='3.1.0', VERSION_COMMENT='Hive release version 3.1.0' where VER_ID=1;
 SELECT 'Finished upgrading MetaStore schema from 3.0.0 to 3.1.0' AS MESSAGE;
