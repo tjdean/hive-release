0304348ed10b32ebd0f89df4d53a3da36d36d413
Bruno Pusztahazi <bpusztahazi@hortonworks.com>
2019-08-06 11:50:01 +0200

    BUG-121341 ADDENDUM
    Change-Id: I7991cda12d970e1476b20c8bd0d7f753ec21c8dd

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -13088,36 +13088,6 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
     return retValue;
   }
 
-  private Map<String, String> convertToAcidByDefault(
-      StorageFormat storageFormat, String qualifiedTableName, List<Order> sortCols,
-      Map<String, String> retValue) {
-    /*for CTAS, TransactionalValidationListener.makeAcid() runs to late to make table Acid
-     so the initial write ends up running as non-acid...*/
-    try {
-      Class inputFormatClass = storageFormat.getInputFormat() == null ? null :
-          Class.forName(storageFormat.getInputFormat());
-      Class outputFormatClass = storageFormat.getOutputFormat() == null ? null :
-          Class.forName(storageFormat.getOutputFormat());
-      if (inputFormatClass == null || outputFormatClass == null ||
-          !AcidInputFormat.class.isAssignableFrom(inputFormatClass) ||
-          !AcidOutputFormat.class.isAssignableFrom(outputFormatClass)) {
-        return retValue;
-      }
-    } catch (ClassNotFoundException e) {
-      LOG.warn("Could not verify InputFormat=" + storageFormat.getInputFormat() + " or OutputFormat=" +
-          storageFormat.getOutputFormat() + "  for " + qualifiedTableName);
-      return retValue;
-    }
-    if(sortCols != null && !sortCols.isEmpty()) {
-      return retValue;
-    }
-    retValue.put(hive_metastoreConstants.TABLE_IS_TRANSACTIONAL, "true");
-    retValue.put(hive_metastoreConstants.TABLE_TRANSACTIONAL_PROPERTIES,
-        TransactionalValidationListener.DEFAULT_TRANSACTIONAL_PROPERTY);
-    LOG.info("Automatically chose to make " + qualifiedTableName + " acid.");
-    return retValue;
-  }
-
   /**
    * Checks to see if given partition columns has DEFAULT or CHECK constraints (whether ENABLED or DISABLED)
    *  Or has NOT NULL constraints (only ENABLED)
